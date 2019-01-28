f9640473101552cf59812c98e5ad1665cf6b5f9b
yiyaohuang <yiyaohuang@tencent.com>
2019-01-28 20:05:18 +0800

    index

---
+++ docs/precache-manifest.4608c8c16b0268a098652a9321740a0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f287162ba55659c14c78",
+    "url": "/static/css/main.cc78b2c2.chunk.css"
+  },
+  {
+    "revision": "f287162ba55659c14c78",
+    "url": "/static/js/main.f287162b.chunk.js"
+  },
+  {
+    "revision": "31b6aa9c82b0de0985e2",
+    "url": "/static/js/1.31b6aa9c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24d53114225990ec07d78b1f987c66ac",
+    "url": "/index.html"
+  }
+];
